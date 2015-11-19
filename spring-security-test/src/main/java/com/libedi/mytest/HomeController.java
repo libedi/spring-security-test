aff03452b61df72efed4723a132c99b1948d5a91
libedi <libedi@gmail.com>
2015-11-19 21:35:38 +0900

    authentication and logout

--- spring-security-test/src/main/java/com/libedi/mytest/HomeController.java
+++ spring-security-test/src/main/java/com/libedi/mytest/HomeController.java
@@ -1,10 +1,15 @@
 package com.libedi.mytest;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.libedi.mytest.dao.MemberInfo;
 
 /**
  * Handles requests for the application home page.
@@ -15,8 +20,19 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value={"/", "/main"})
-	public String main(Model model){
+	public String main(HttpServletRequest request, Model model){
 		logger.info("main");
+		
+		Authentication auth = (Authentication)request.getUserPrincipal();
+		String name = "";
+		if(auth != null){
+			Object principle = auth.getPrincipal();
+			if(principle != null && principle instanceof MemberInfo){
+				name = ((MemberInfo)principle).getName();
+			}
+		}
+		model.addAttribute("name", name);
+		
 		return "main";
 	}
 	
--- spring-security-test/src/main/webapp/WEB-INF/views/main.jsp
+++ spring-security-test/src/main/webapp/WEB-INF/views/main.jsp
@@ -8,6 +8,7 @@
 <title>메인화면</title>
 </head>
 <body>
+	<sec:authorize access="isAnonymous()">
 	<form id="loginfrm" name="loginfrm" method="post" action="/j_spring_security_check">
 		<table>
 			<tr>
@@ -29,5 +30,30 @@
 			</tr>
 		</table>
 	</form>
+	</sec:authorize>
+	
+	<sec:authorize access="isAuthenticated()">
+	${name}님 반갑습니다.
+	
+	<a href="/j_spring_security_logout">로그아웃</a>
+	</sec:authorize>
+	
+	<ul>
+		<sec:authorize access="hasRole('ROLE_ADMIN')">
+		<li>관리자 화면</li>
+		</sec:authorize>
+		
+		<sec:authorize access="permitAll">
+		<li>비회원게시판</li>
+		</sec:authorize>
+		
+		<sec:authorize access="isAuthenticated()">
+		<li>준회원게시판</li>
+		</sec:authorize>
+		
+		<sec:authorize access="hasAnyRole('ROLE_MEMBER2','ROLE_ADMIN')">
+		<li>정회원게시판</li>
+		</sec:authorize>
+	</ul>
 </body>
 </html>
